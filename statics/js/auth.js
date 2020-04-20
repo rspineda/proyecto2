789b480a305326ca402117aa1372980811328b6b
Ronald <ronystivens@hotmail.com>
2020-04-20 22:27:31 +0200

    limpiando código

--- statics/js/auth.js
+++ statics/js/auth.js
@@ -1,14 +1,12 @@
 
-//Registro de un nuevo usuario mediante usuario y contraseña
 document.getElementById("send").addEventListener("click", registro);
 function registro(){
-  //const name = document.getElementById("name").value;
-  //const surname = document.getElementById("surname").value;
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((data)=>{
-    //me creo un registro en la database con el uid de cada nuevo usuario
+    
     console.log(data.user.uid); 
     const datos = {
       nombre: document.getElementById("name").value,
@@ -27,23 +25,19 @@ function registro(){
   .catch((error)=> {
     let errorCode = error.code;
     let errorMessage = error.message;
-    //console.log(errorCode+"("+errorMessage+")");
     console.log(`${errorCode} ("${errorMessage}")`);
   }); 
 }
 
-//acceso mediante usuario y contraseña:
 document.getElementById("loginEmail").addEventListener("click", accesoRegistrado); 
 function accesoRegistrado(){
     const email = document.getElementById("logEmail").value;
     const password = document.getElementById("logPassword").value;
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((user)=>{
-      //console.log(user);
       console.log(`${user.email} esta logueado`)
     })
     .catch((error) => {
-      // Handle Errors here.
       let errorCode = error.code;
       let errorMessage = error.message;
       console.log(error.code);
@@ -55,54 +49,33 @@ function accesoRegistrado(){
     });
 }
 
-//Log out de usuarios
 document.getElementById("logout").addEventListener("click", salir);
 function salir(){
     firebase.auth().signOut().then( () => {
-      console.log("Deslogado") // Sign-out successful.
+      console.log("Deslogado")
     }).catch( (error) => {
-        console.log("Error en el signOut") // An error happened.
+        console.log("Error en el signOut") 
     });
 };
 
-//para ver los usuarios conectados en ese momento
+
 firebase.auth().onAuthStateChanged((user)=> {  
   if (user) {
-    // User is signed in.
     console.log(`el ususario ${user.email} está conectado`)
   } else {
-    // No user is signed in.
     console.log(`Ningun usuario logueado`)
   }
 });
 
-//Para restablacer la contraseña
 document.getElementById("getPass").addEventListener("click", restablecerContraseña);
 function restablecerContraseña(){
   let auth = firebase.auth();
   let emailAddress = document.getElementById("logEmail").value;
   
   auth.sendPasswordResetEmail(emailAddress).then(function() {
-    // Email sent.
     console.log("se ha enviado un email de restablacimiento de contraseña");
   }).catch(function(error) {
-    // An error happened.
     console-log("error al restablecer la contraseña");
   });
 }
 
-/*
-//Para dar de baja a un usuario (estando logueado) Si no está logueado pondré una alternativa mediante email
-let user = firebase.auth().currentUser;
-document.getElementById("").addEventListener("click", eliminarUsuario);
-function eliminarUsuario(){
-  user.delete().then(function() {
-    // User deleted.
-    console.log(`el usuario ${user} ha sido eliminado de la plataforma`);
-    //Podría implementar aquí tambien el borrar sus datos (perfil académico) de la base de datos
-  }).catch(function(error) {
-    // An error happened.
-    console.log(`ha ocrrido un error al eliminar el usuario ${user}, intenta con logout y login nuevamente para reutenticar`);
-  });
-}
-*/
--- statics/js/index.js
+++ statics/js/index.js
@@ -1,6 +1,6 @@
-// My web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDEwX2nyLNbYQMiwmeC6wHATE4xzPE6S58",
+    apiKey: "",
     authDomain: "alumnos-a6801.firebaseapp.com",
     databaseURL: "https://alumnos-a6801.firebaseio.com",
     projectId: "alumnos-a6801",
@@ -9,12 +9,10 @@ const firebaseConfig = {
     appId: "1:958924277619:web:0af491a9fd69d75dc7e706"
   };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-//Accedo a la base de datos:
 const database = firebase.database();
 
-//Extraigo el los datos del alumno desde el DOM y los subo al documento
+
 document.getElementById("send").addEventListener("click", registro);
 function registro(){
     const name = document.getElementById("name").value;
@@ -26,7 +24,7 @@ function registro(){
     const comments = document.getElementById("comments").value;
     const counseling = document.getElementById("yes").value;
     const key = `${name} ${surname}`;
-    
+
     const perfil = {
         nombre : name,
         apellido : surname,
@@ -42,10 +40,9 @@ function registro(){
         carrera: degree 
     }
 
-    //accedo a la referencia de la  base de datos
     const refPerfil = database.ref(`perfiles/${key}`);
     const refEspecialidades = database.ref(`especialiades/${key}`);
-    //almaceno los datos
+
     refPerfil.set(perfil)
     .then(()=>{
         console.log('Perfil del alumno guardado correctamente')
@@ -61,41 +58,16 @@ function registro(){
     .catch(()=>{
         console.log(`error al guardar especialidad`);
     })
-
-
 }
-//resfrescar la pagina o borrar los datos
+
 document.getElementById("reset").addEventListener("click",()=>{location.reload()});
 
-/*
-//carga de los alumnos registrados por medio de un recorrido de los hijos
-const ref = database.ref()
-ref.on('value', (snapshot)=>{
-    snapshot.forEach((childsnapshot)=>{
-        console.log(childsnapshot.val());
-    })
-});
-*/
+
 const ref = database.ref()
 ref.on('child_added', (snapshot)=>{
     console.log(snapshot.val());
 });
 
-
-/*
-//eliminar un alumno
-document.getElementById("").addEventListener("click", eliminar);
-function eliminar(){
-let refalumno = database.ref(key);
-refalumno.remove()
-.then(()=>{
-
-})
-.catch(()=>{
-
-})
-}
-*/
 
 
 
